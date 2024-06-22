9c145a6a97c888e7bf04ecf1211b35f54531b27a
Javier Vinueza <javiervin@hotmail.com>
2024-06-22 04:39:04 -0500

    feat: Add finished-animation, mode-toggle, and theme-provider components. Implement animation and dark mode toggle functionality.

---
+++ src/ui/common/components/finished-animation.tsx
@@ -0,0 +1,15 @@
+// import Lottie from 'react-lottie';
+// import animationData from '@/path/to/your/lottie-file.json';
+
+// export default function FinishedAnimation() {
+//   const defaultOptions = {
+//     loop: true,
+//     autoplay: true,
+//     animationData: animationData,
+//     rendererSettings: {
+//       preserveAspectRatio: 'xMidYMid slice',
+//     },
+//   };
+
+//   return <Lottie options={defaultOptions} height={100} width={100} />;
+// }
