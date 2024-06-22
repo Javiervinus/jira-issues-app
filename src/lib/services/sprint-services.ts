a540b132491d4d48c1fea71fcc081f5dd9f0f9d1
Javier Vinueza <javiervin@hotmail.com>
2024-06-22 15:11:43 -0500

    feat: Add functions to fetch and process sprint data
    Added functions to fetch the latest sprints and current sprint issues from Jira API. Implemented logic to categorize and count issues by assignee status.

--- src/lib/services/sprint-services.ts
+++ src/lib/services/sprint-services.ts
@@ -1,24 +1,125 @@
+import { GetIssuesResponse } from "@/core/interfaces/get-issues-response";
 import { GetSprintsResponse } from "@/core/interfaces/get-sprints-response";
+import { IssueByAssignee } from "@/core/interfaces/issues-by-assignee.interface";
+import { Issue } from "@/core/interfaces/issues.interface";
+const baseUrl = "https://betterplan.atlassian.net/rest/agile/latest/";
 
-export class SprintServices {
-  static baseUrl = "https://betterplan.atlassian.net/rest/agile/latest/";
+export async function getLastSprints(): Promise<GetSprintsResponse> {
+  //  create query params for the request with URLSearchParams
+  const params = new URLSearchParams();
+  params.append("state", "active,future");
+  params.append("maxResults", "2");
+  const query = params.toString();
 
-  static async getLastSprints(): Promise<GetSprintsResponse> {
-    //  create query params for the request with URLSearchParams
-    const params = new URLSearchParams();
-    params.append("state", "active,future");
-    params.append("maxResults", "2");
-    const query = params.toString();
+  //  make the request to the Jira API
+  const res = await fetch(`${baseUrl}/board/2/sprint?${query}`, {
+    headers: {
+      Authorization: `Basic ${Buffer.from(
+        `${process.env.JIRA_USER}:${process.env.JIRA_TOKEN}`
+      ).toString("base64")}`,
+    },
+  });
 
-    //  make the request to the Jira API
-    const res = await fetch(`${this.baseUrl}/board/2/sprint?${query}`, {
+  return res.json();
+}
+export async function getCurrentSprintIssues(sprintId: number) {
+  const params = new URLSearchParams();
+  params.append("jql", "project = 'Betterplan'");
+  params.append(
+    "fields",
+    "status,assignee,issuetype,issuenum,issuenum,subtasks"
+  );
+  const query = params.toString();
+
+  const res = await fetch(
+    `${baseUrl}/board/2/sprint/${sprintId}/issue?${query}`,
+    {
       headers: {
         Authorization: `Basic ${Buffer.from(
           `${process.env.JIRA_USER}:${process.env.JIRA_TOKEN}`
         ).toString("base64")}`,
       },
-    });
+      cache: "no-cache",
+    }
+  );
 
-    return res.json();
-  }
+  const response = (await res.json()) as GetIssuesResponse;
+  return getIssuesByAssignee(response.issues);
+}
+function getIssuesByAssignee(issues: Issue[]) {
+  const issuesByAssignee: IssueByAssignee[] = [];
+  issues.forEach((issue) => {
+    const assignee = issue.fields.assignee;
+    const assigneeIndex = issuesByAssignee.findIndex(
+      (assigneeIssues) =>
+        assigneeIssues.assignee.emailAddress === assignee.emailAddress
+    );
+    let addIssue = false;
+    if (assigneeIndex === -1) {
+      const issueByAssignee: IssueByAssignee = {
+        assignee,
+        issues: [],
+        doneIssues: 0,
+        totalIssues: 0,
+        inProgressIssues: 0,
+      };
+      // se añaden las tareas que no son subtasks
+      if (
+        issue.fields.subtasks.length === 0 &&
+        issue.fields.issuetype.subtask === false
+      ) {
+        addIssue = true;
+      }
+      if (
+        issue.fields.subtasks.length === 0 &&
+        issue.fields.issuetype.subtask === true
+      ) {
+        addIssue = true;
+      }
+      if (addIssue) {
+        issueByAssignee.totalIssues++;
+        if (
+          issue.fields.status.name === "En Dev" ||
+          issue.fields.status.name === "Finalizada"
+        ) {
+          issueByAssignee.doneIssues++;
+        } else if (issue.fields.status.name === "En curso") {
+          issueByAssignee.inProgressIssues++;
+        }
+        issueByAssignee.issues.push(issue);
+      }
+
+      issuesByAssignee.push(issueByAssignee);
+    } else {
+      if (
+        issue.fields.subtasks.length === 0 &&
+        issue.fields.issuetype.subtask === false
+      ) {
+        addIssue;
+      } else {
+        if (
+          issue.fields.subtasks.length === 0 &&
+          issue.fields.issuetype.subtask === true
+        ) {
+          addIssue = true;
+        }
+      }
+      if (addIssue) {
+        const issueByAssignee = issuesByAssignee[assigneeIndex];
+        issueByAssignee.totalIssues++;
+        if (
+          issue.fields.status.name === "En Dev" ||
+          issue.fields.status.name === "Finalizada"
+        ) {
+          issueByAssignee.doneIssues++;
+        } else if (issue.fields.status.name === "En curso") {
+          issueByAssignee.inProgressIssues++;
+        }
+
+        issueByAssignee.issues.push(issue);
+      }
+    }
+  });
+
+  return issuesByAssignee;
 }
