09343bf03f09f38d67dbdeb261a4f5566d1956f0
Javier Vinueza <javiervin@hotmail.com>
2024-06-26 05:26:21 -0500

    Refactor hover card links for responsive design
    Updated link classes and URLs for better responsiveness.

--- src/ui/common/components/IssuesHoverCard.tsx
+++ src/ui/common/components/IssuesHoverCard.tsx
@@ -6,8 +6,15 @@ export default function IssuesHoverCard({ issues }: { issues: Issue[] }) {
       {issues.map((issue) => (
         <li key={issue.key}>
           <a
-            className=" hover:underline hover:text-blue-500 "
+            className=" hover:underline hover:text-blue-500 md:block hidden"
             href={`https://betterplan.atlassian.net/jira/software/c/projects/BT/boards/2?selectedIssue=${issue.key}`}
+            target="_blank"
+          >
+            {issue.key}
+          </a>
+          <a
+            className=" hover:underline hover:text-blue-500 md:hidden block"
+            href={`https://betterplan.atlassian.net/browse/${issue.key}`}
             target="_blank"
           >
             {issue.key}
